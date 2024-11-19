bb31f514c3935b2b9e320bda358e5a9426f18972
Navdisenyo <navdisenyo@gmail.com>
2024-11-19 19:43:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Navdisenyo - eCommerce & Blockchain Development",
+  description: "eCommerce, crypto-exchange & Blockchain Development services provider.",
 };
 
 export default function RootLayout({
