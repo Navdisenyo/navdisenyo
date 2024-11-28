cfa87ced85fe5161a0bafd91c45cd4c9940f3bfc
Navdisenyo <navdisenyo@gmail.com>
2024-11-28 12:50:15 +0530

    Add files via upload

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer = () => {
           <SocialPin />
           <SocialYoutube />
         </div>
-        <p className='mt-6 '>&copy; 2024 Navdisenyo, Inc. All rights reserved.</p>
+        <p className='mt-6 '>&copy; 2024 Navdisenyo. All rights reserved.</p>
       </div>
     </section>
   );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
         Streamline your workflow & Boost Your productivity with us.
       </p>
       <div className='inline-flex gap-1 items-center'>
-        <p>Get started for free, book an apportment.</p>
+        <p>Get started for free, whatsapp us: +91 974-828-4985</p>
         <AarrowRight className="h-4 w-4 inline-flex justify-center items-center" />
       </div>
     </div>
@@ -25,12 +25,12 @@ export const Header = () => {
           <Image src={Logo} alt="navdisenyo" height={40} width={40} />
           <MenuIcon className="h-5 w-5 md:hidden" />
           <nav className='hidden md:flex gap-6 text-black/60 items-center'>
-            <a href='/'>Home</a>
+            <a href='/' className='text-cyan-900'>Home</a>
             <a href='#'>Features</a>
             <a href='#'>Customers</a>
-            <a href='/services'>Services</a>
+            <a href='#'>Services</a>
             <a href='#'>Contact</a>
-            <button className="inline-flex h-9 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"><a href="https://koalendar.com/e/product-demo-navdissenyo">Apportment</a></button>
+            <button className="inline-flex h-9 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"><a href="https://navdisenyo1.zohobookings.com/#/navdisenyo">Appointment</a></button>
           </nav>
         </div>
       </div>
